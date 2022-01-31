4d373bbbb8eff398057e863e707b4e93dc920f45
Gospodi <gospodijazzet@gmail.com>
2022-01-31 13:18:22 +0300

    add action Button + constants for timer

--- EggTimer App/ViewController.swift
+++ EggTimer App/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
+    @IBAction func softnessSelected(_ sender: UIButton) {
+        print(sender.currentTitle)
     }
-
+    
 
 }
 
